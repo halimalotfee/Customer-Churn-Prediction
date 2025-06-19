d03ad90417dc3c9a77357342ca4b5f8ea46b2350
Halima Lotfe <haima.lotfe06@gmail.com>
2025-06-19 15:22:02 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Customer-Churn-Prediction",
+    version="0.0.0",
+    author="halimalotfee",
+    author_email="halima.lotfe06@gmail.com",
+    packages=find_packages()
+)
