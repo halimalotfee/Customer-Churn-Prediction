c00c45461465d549bf7a94a779ca05d39999908a
Halima Lotfe <haima.lotfe06@gmail.com>
2025-06-21 21:53:02 +0200

    data ingestion

---
+++ CustomerChurn/entity/config_entity.py
@@ -0,0 +1,29 @@
+import os
+from CustomerChurn.constants import *
+from dataclasses import dataclass
+from datetime import datetime
+
+# Générer un timestamp pour l'archivage
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+
+# Configuration principale du pipeline d'entraînement
+@dataclass
+class TrainingPipelineConfig:
+    pipeline_name: str = PIPELINE_NAME
+    artifact_dir: str = os.path.join(ARTIFACT_DIR, TIMESTAMP)
+    timestamp: str = TIMESTAMP
+
+# Instance globale de la configuration du pipeline
+training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
+
+
+# Configuration de la phase d'ingestion des données
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME)
+    feature_store_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME)
+    training_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
+    testing_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
+    train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+    collection_name: str = DATA_INGESTION_COLLECTION_NAME
--- demo.py
+++ demo.py
@@ -1,10 +1,14 @@
 from CustomerChurn.logger import logging 
 from CustomerChurn.exception import ChurnException
 import sys
-logging.info("Démarrage du programme.")
-logging.warning("Ceci est un avertissement.")
-logging.error("Une erreur s'est produite.")
-try:
-    a=2/0
-except Exception as e:
-    raise ChurnException(e,sys)
+# logging.info("Démarrage du programme.")
+# logging.warning("Ceci est un avertissement.")
+# logging.error("Une erreur s'est produite.")
+# try:
+#     a=2/0
+# except Exception as e:
+#     raise ChurnException(e,sys)
+from CustomerChurn.pipline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
